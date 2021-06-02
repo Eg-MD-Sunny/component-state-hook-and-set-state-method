ee8d6cb2274509d6dd0d2167ea9fe5d3c1824cf8
Naim Zaman <egnaimur@gmail.com>
2021-06-03 00:55:34 +0600

    Using useState create simple Counter Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Counter Project</p>
+				<Counter></Counter>			{/*<<========Component Call*/}
+			</header>
+		</div>
+	);
 }
-
+function Counter(){
+	const [count, setCount] = useState(0)  //<<=========useState Declare
+	const handleIncrease = () =>{		  //<<==========handleIncrease function create [Start]
+		console.log("Yes! I'm Increase");
+		let newCount = count + 1;
+		setCount(newCount);
+	}									 //<<==========handleIncrease function create [End]
+	const handleDecrease = () =>{		//<<==========handleDecrease function create [Start]
+		console.log("Yes! I'm Decrease");
+		if(count <= 0) {
+			return;
+		  } else {
+			setCount(count - 1);
+		  }
+	}								  //<<==========handleDecrease function create [End]
+	return(
+		<div>
+			<h1>Count: {count}</h1>
+			<button onClick={handleIncrease}>Increase</button>	{/*<<=======eventHandle Connect In Button*/}
+			<button onClick={handleDecrease}>Decrease</button>
+		</div>
+	)
+}
 export default App;
